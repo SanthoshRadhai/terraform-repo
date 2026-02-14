dfc10983ea086a0a59b92bbc18981d151f1b5295
poeagetechnology <santhoshradhai2005@gmail.com>
2026-02-14 14:44:20 +0530

    fixing secrity group : removing the old

--- main.tf
+++ main.tf
@@ -12,44 +12,17 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
 data "aws_vpc" "default" {
   default = true
 }
+
 data "aws_subnets" "default_in_vpc" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
-resource "aws_security_group" "ec2_sg" {
-  name        = "terraform-ec2-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = data.aws_vpc.default.id
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_cidr]
-  }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "terraform-ec2-sg"
-  }
-}
-
 
 resource "aws_instance" "ec2" {
   ami                         = var.ami_id
@@ -63,4 +36,3 @@ resource "aws_instance" "ec2" {
     Name = "terraform-ec2"
   }
 }
-
